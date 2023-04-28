128ed4d19ece48a67bf5204f2940ca81036eb607
SailboatHC <lokeshchandrasaiyerukonda@gmail.com>
2023-04-28 15:31:04 +0530

    add: mobile nav created

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import '../styles/style.css';
 import '../styles/mediaqueries/header.css';
-import '../styles/mediaqueries/login.css'
+import '../styles/mediaqueries/login.css';
+import '../styles/mobile-nav.css';
+
+import mobileNav from './utils/mobile-nav';
+
+mobileNav();
